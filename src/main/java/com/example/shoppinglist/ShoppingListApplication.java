58d88012ffb5e0936132a6258dcc2472477e5b97
AMDRO <user@MacBook-Pro-user.local>
2018-09-22 17:53:04 +0200

    Get, post, put, delete note from database

---
+++ src/main/java/com/example/shoppinglist/ShoppingListApplication.java
@@ -0,0 +1,14 @@
+package com.example.shoppinglist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class ShoppingListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingListApplication.class, args);
+	}
+}
